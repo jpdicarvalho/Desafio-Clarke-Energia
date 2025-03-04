15ee2d7596afc8719b2c2d0e6c5e89514a8df434
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-03-04 20:12:02 -0300

    handler erros

--- frontend/src/SearchSupplier/SearchSupplier.jsx
+++ frontend/src/SearchSupplier/SearchSupplier.jsx
@@ -1,19 +1,18 @@
-import { useQuery, useLazyQuery } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 import { gql } from "../config/apolloClient";
+import { useState, useEffect } from 'react';
+import { motion } from "framer-motion";
 
 import './SearchSupplie.css'
 
 import Logo from '../assets/image.png'
 import image_section from '../assets/image-section.png'
-import image_example from '../assets/energy.jpg'
 
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { MdOutlineStar } from "react-icons/md";
 import { BsBarChartFill } from "react-icons/bs";
 import { FaUsers } from "react-icons/fa6";
-
-import { useState } from 'react';
-import { motion } from "framer-motion";
+import { VscError } from "react-icons/vsc";
 
 const GET_FORNECEDORES = gql`
     query GetFornecedores($consumo: Int!) {
@@ -34,17 +33,33 @@ const GET_FORNECEDORES = gql`
 function SearchSupplie () {
     const [inputEnergia, setInputEnergia] = useState (false);
     const [valorConsumo, setValorConsumo] = useState ("");
+    const [erroValue, setErroValue] = useState (false);
+    const [showErroRequest, setShowErroRequest] = useState(false);
     const [buscarFornecedores, { data, loading, error }] = useLazyQuery(GET_FORNECEDORES);
 
 
+    //Função para lidar com a busca de fornecedores
     const handleBuscarFornecedores = () => {
-        const consumo = parseInt(valorConsumo, 10); // Convertendo string para número
-        if (isNaN(consumo) || consumo <= 0) {
-            alert("Por favor, insira um valor válido de consumo.");
-            return;
+        const consumo = parseInt(valorConsumo, 10);
+        if (isNaN(consumo) || consumo <= 0) { // Verificação para caso a regex do input deixe passar algo
+            return setErroValue(true); // Seta a variável 'erroValue' como true e oculta o btn de busca
         }
-        buscarFornecedores({ variables: { consumo } });
+        buscarFornecedores({ variables: { consumo } }); // Chama a consulta GraphQL 'GET_FORNECEDORES', passando 'consumo' como variável
+
     };
+
+    // Atualiza o estado para exibir a mensagem quando houver erro
+    useEffect(() => {
+        if (error) {
+            setShowErroRequest(true);
+            // Define um tempo para ocultar a mensagem após 5 segundos (5000ms)
+            const timer = setTimeout(() => {
+                setShowErroRequest(false);
+            }, 3000);
+
+            return () => clearTimeout(timer); // Limpa o timeout quano o componente for desmontado
+        }
+    }, [error]);
 
     return (
         <>
@@ -53,6 +68,7 @@ function SearchSupplie () {
                     <div className='box__image'>
                         <img className='logo' src={Logo} alt="" />
                     </div>
+                    
                 </div>
                 <div className='section__main'>
                     <div className="get__started">
@@ -63,15 +79,12 @@ function SearchSupplie () {
                         </p>
 
                         {!inputEnergia && (
-                            <button 
-                                className="btn__get__started" 
-                                onClick={() => setInputEnergia(true)}
-                            >
+                            <button className="btn__get__started" onClick={() => setInputEnergia(true)}>
                                 <p>Encontrar fornecedor</p>
                                 <IoIosArrowRoundForward className="icon__IoIosArrowRoundForward" />
                             </button>
                         )}
-
+                        
                         {inputEnergia && (
                             <motion.div 
                                 className="input-wrapper"
@@ -85,28 +98,51 @@ function SearchSupplie () {
                                     name="text" 
                                     type="text" 
                                     value={valorConsumo}
-                                    onChange={(e) => setValorConsumo(e.target.value)}
+                                    onChange={(e) => {
+                                        const valor = e.target.value;
+                                        // Regex: Permite apenas números, não permite "0" isolado, mas aceita "10", "100" etc.
+                                        if (valor === "" || /^(?!0$)\d+$/.test(valor)) {
+                                            setValorConsumo(valor);
+                                        }
+                                    }}
                                 />
-                                <button 
-                                    className="Search-btn"
-                                    onClick={handleBuscarFornecedores}
-                                >
-                                    Encontrar
-                                </button>
+                                {loading ? (
+                                    <div className="spinner"></div>
+                                ):(
+                                    <>
+                                        {valorConsumo && !erroValue &&(
+                                            <button className="Search-btn" onClick={handleBuscarFornecedores}>
+                                                Encontrar
+                                            </button> 
+                                        )}
+                                    </>
+                                         
+                                )}
+                               
                             </motion.div>
+                             
                         )}
+                        
                     </div>
 
                     <div className='image__section'>
                         <img className='inner_image_section' src={image_section} alt="" />
                     </div>
 
-                    
                 </div>
+
                 {/* Exibindo os fornecedores */}
                 <div className="result__section">
-                    {loading && <p>Buscando fornecedores...</p>}
-                    {error && <p>Erro ao buscar fornecedores: {error.message}</p>}
+                    {loading &&
+                        <p>Buscando fornecedores...</p>
+                    }
+                    {showErroRequest &&
+                        <p className="message__erro__request">
+                            <VscError className="icon__VscError"/><br/>
+                            Hummhumm...<br />Parece que houve um problema ao realizar a busca. Tente novamente mais tarde.
+                        </p>
+                    }
+                    {/* Falta tratar essa situação */}
                     {data && data.fornecedores.length === 0 && <p>Nenhum fornecedor encontrado.</p>}
                     {data && data.fornecedores.map((fornecedor) => (
                         
