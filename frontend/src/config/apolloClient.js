2bc67d435f7982037f4cec9f82d53118a8e4a2f5
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-03-05 12:18:32 -0300

    add url api-production

--- frontend/src/config/apolloClient.js
+++ frontend/src/config/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 const client = new ApolloClient ({
-    uri: "http://localhost:4000/",
+    uri: "https://desafio-clarke-energia-api-graphql.up.railway.app/",
     cache: new InMemoryCache(),
 });
 
